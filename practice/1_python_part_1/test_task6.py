637ecfa02e0ac1d59cd63785d373e93ebc3a253e
Aliaksandr Kadzetau <akadzetau@griddynamics.com>
2023-01-21 19:36:35 +0100

    1_python_part_1 test1-6 fix1

--- practice/1_python_part_1/test_task6.py
+++ practice/1_python_part_1/test_task6.py
@@ -5,5 +5,5 @@ from task6 import get_min_max
 class TestTask1(TestCase):
     def test1(self):
         self.expected = (-2, 34)
-        self.result = get_min_max("../resources/1_pyhon_part_1_task6.txt")
+        self.result = get_min_max("files/1_pyhon_part_1_task6.txt")
         self.assertTupleEqual(self.result, self.expected)
